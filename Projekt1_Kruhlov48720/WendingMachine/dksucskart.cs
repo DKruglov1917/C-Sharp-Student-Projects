2a8ceaca066f0abbbc861a0468a549ad39911394
DKruglov1917 <72298930+DKruglov1917@users.noreply.github.com>
2021-03-17 19:12:22 +0100

    Add files via upload

---
+++ Projekt1_Kruhlov48720/WendingMachine/dksucskart.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WendingMachine
+{
+    public partial class dksucskart : Form
+    {        
+        public dksucskart()
+        {
+            InitializeComponent();
+        }
+
+        private void prod_name_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
